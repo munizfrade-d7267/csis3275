703c88d107ba2496d6cff57f8d0ce7d17fb6efd2
fradys <fradys@gmail.com>
2020-05-20 23:33:52 -0700

    First version

---
+++ MyJavaProject/src/myjavaproject/MyJavaProject.java
@@ -0,0 +1,20 @@
+
+package myjavaproject;
+
+/**
+ *
+ * @author Daniel Muniz Frade - 300247267
+ * 
+ */
+public class MyJavaProject {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Hello from MyJavaProject!");
+    }
+    
+}
+
+
+
+  
