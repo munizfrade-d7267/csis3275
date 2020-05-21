87fa1bee48167156363a2e8a6d75ec82998e7de7
fradys <fradys@gmail.com>
2020-05-20 23:46:49 -0700

    Fourth version

--- MyJavaProject/src/myjavaproject/MyJavaProject.java
+++ MyJavaProject/src/myjavaproject/MyJavaProject.java
@@ -11,7 +11,7 @@ public class MyJavaProject {
     
     public static void main(String[] args) {
         System.out.println("Hello from MyJavaProject!");
-        
+        System.out.println("Random number: " + Math.random());
     }
     
 }
